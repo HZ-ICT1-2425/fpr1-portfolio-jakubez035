4522e4ffd4c4c1ba260c122625584747875324c0
Jakub <holikj1@gmail.com>
2025-02-13 09:13:54 +0100

    Changed vite.config.js, removed stylesheets, using @vite now, moved css and js back to resources, because of @vite. Created a main layout for pages, added home.css for index.blade.php.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+                'resources/css/skeleton.css',
+                'resources/css/home.css',
+                'resources/js/homepageimages.js'],
             refresh: true,
         }),
     ],
